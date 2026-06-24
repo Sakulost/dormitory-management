3b06931503205c9cdfc3307cd193097865bfb5f2
Sakulost <sakulost@users.noreply.github.com>
2026-06-25 00:38:45 +0800

    更新仓库名为 dormitory-management

--- dormitory-frontend/vite.config.js
+++ dormitory-frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/宿舍管理系统/',
+  base: '/dormitory-management/',
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
